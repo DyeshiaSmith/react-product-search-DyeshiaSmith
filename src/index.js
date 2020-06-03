df47a80f14e254633362c2a05444e5fa66c80e61
D <dyeshiamonet@gmail.com>
2020-06-03 16:07:01 -0500

    add as a one line arrow fxn

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import React from "react";
+//imports like ^^ means its coming from the node modules.
 import ReactDOM from "react-dom";
 import "./index.css";
+
+//we must destructor any named export
 import { App } from "./App";
 
+//use to pull in html
 ReactDOM.render(
   <React.StrictMode>
     <App />
