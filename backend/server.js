9b7ada73a82ea8f492772d6ef1265cb099ff2c88
D <dyeshiamonet@gmail.com>
2020-06-16 18:32:30 -0500

    get hello world running from express server

---
+++ backend/server.js
@@ -0,0 +1,10 @@
+import express from "express";
+
+const app = express();
+const PORT = 80;
+
+app.get("/", (_, res) => res.end("<p>Hello from express</p>"));
+
+app.listen(PORT, () => {
+  console.info(`running a server on port: ${PORT}`);
+});
